59fd412596dc3f9002fc365bfbe9e81456f2a858
LsDuke <36623512+LsDuke@users.noreply.github.com>
2018-02-22 02:06:56 +0100

    Add files via upload

--- yoshi/blog/models.py
+++ yoshi/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post',related_name='comments')
-    author = models.Charfield(max_length=200)
+    author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
     validation_commentaire = models.BooleanField(default=False)
--- yoshi/blog/views.py
+++ yoshi/blog/views.py
@@ -1,5 +1,54 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView,
+                                    DetailView, CreateView,
+                                    UpdateView, DeleteView     )
 
 # Create your views here.
 
-#BITCHHHHHH mother fucker
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post #lier à un modele
+
+
+    def get_Queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    #je ne veux pas que tout le monde puisse créer ma view d'ou le decorateur pour CBV mixin
+    login_url = '/login/'
+    redirect_field_name = '/blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = '/blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    #utilisation du reverse_lazy pour attendre que le delete soit effectué avant
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url='/login/'
+    redirect_field_name = '/blog/post_detail.html'
+    model = Post
+
+    def get_Queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
